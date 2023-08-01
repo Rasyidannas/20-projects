439f261ae181facd9eb8fc8bafc8d32d34cf3353
rasyid annas <rasshit.dsgn@gmail.com>
2023-08-01 10:15:06 +0700

    10/6. Form Values.mp4

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -2,6 +2,19 @@ const inputContainer = document.getElementById("input-container");
 const countdownFowm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = '';
+let countdownDate = '';
+
 //Set Date Input min with Toady's Date
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+function updateCountDown (e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+//Event Listener for form
+countdownFowm.addEventListener('submit', updateCountDown);
